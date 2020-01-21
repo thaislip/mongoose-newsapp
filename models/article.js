c2b7685ddd0c7d983de973c4704159de05e14584
thaislip <taber.haislip@gmail.com>
2020-01-21 13:15:57 -0500

    cannot GET/readArticle; articles will not populate on the page, but shows up in the terminal; style sheet getting a 404 error.

--- models/article.js
+++ models/article.js
@@ -2,19 +2,20 @@ var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 var ArticleSchema = new Schema({
-    title: {
-        type: String,
-        require: true
-    },
-    link: {
-        type: String,
-        required: true
-    },
-    comment: [{
-        type: Schema.Types.ObjectId,
-        ref:"connect"
-    }]
+  title: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  comment: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Comment"
+    }
+  ]
 });
-
-var Article = mongoose.model("Article". ArticleSchema);
+var Article = mongoose.model("Article", ArticleSchema);
 module.exports = Article;
--- server.js
+++ server.js
@@ -19,15 +19,21 @@ app.engine("handlebars", hb({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
 mongoose.connect("mongodb://localhost/mongoose-newsapp")
-var db = mongoose.connection;
+// const MONGODB_URI= process.env.MONGODB_URI || "mongodb://localhost/mongoose-newsapp";
+// mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
 
+
+var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function() {
     console.log("connected to mongoose!");
 });
  
+var routes = require("./controller/controller");
+app.use("/", routes);
 
-var port = process.env.PORT || 3000;
+
+var port = process.env.PORT || 8080;
 app.listen(port, function(){
     console.log("listening on PORT " + port);
 }); 
